8f1cd5c124020b27a9eb83b2436d58c12907cb6f
William Bay <william@flauntyoursite.com>
2018-09-18 23:58:26 -0700

    Fixed issue causing Login.JS not to load.

--- login/class-flaunt-sites-core-login.php
+++ login/class-flaunt-sites-core-login.php
@@ -1,15 +1,13 @@
-<?php 
+<?php
+/**
+ * Functions for the Login experience.
+ */
+function fsc_custom_login() {
 
-    function fsc_custom_login() {
+    wp_enqueue_style( 'fsc-custom-login', plugin_dir_url( __FILE__ ) . 'css/class-flaunt-sites-core-login.css', '', '201800618' );
+    wp_enqueue_script( 'fsc-custom-login', plugin_dir_url( __FILE__ ) . 'js/class-flaunt-sites-core-login.js', '', '201800618', true );
+    wp_enqueue_script( 'wp-api' );
+    wp_localize_script( 'fsc-custom-login', 'flaunt_sites_custom_login_ajaxify', array( 'currentSite' => get_bloginfo( 'url' ) ) );
 
-        wp_enqueue_style( 'fsc-custom-login', plugin_dir_url(  __FILE__  ) . 'css/class-flaunt-sites-core-login.css' );
-        wp_enqueue_script( 'fsc-custom-login', plugin_dir_url(  __FILE__  ) . 'js/class-flaunt-sites-core-login.js' );
-        wp_enqueue_script( 'wp-api' );
-
-        wp_localize_script( 'fsc-custom-login', 'flaunt_sites_custom_login_ajaxify', array( 'currentSite' => get_bloginfo( url ) ) );
-    }
-
-    add_action( 'login_enqueue_scripts', 'fsc_custom_login' );
-    
-
-    
+}
+add_action( 'login_enqueue_scripts', 'fsc_custom_login' );
