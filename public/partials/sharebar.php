fe1c38a6c959c30982dac5c555bed91eda59a976
William Bay <william@flauntyoursite.com>
2024-02-17 16:34:56 -0800

    Removed Sharebar markup.

--- public/partials/sharebar.php
+++
@@ -1,7 +0,0 @@
-<div class="fs-sharebar">
-<span>Share this photo:</span>
-<a href="https://www.facebook.com/sharer.php?u=<?php the_permalink(); ?>"><svg class="fs-icons"><use xlink:href="#icon-facebook-square"></use></svg></a>									
-<a href="#"><svg class="fs-icons"><use xlink:href="#icon-twitter-square"></use></svg></a>
-<a href="#"><svg class="fs-icons"><use xlink:href="#icon-instagram-square"></use></svg></a>									
-<a href="#"><svg class="fs-icons"><use xlink:href="#icon-pinterest-square"></use></svg></a>																		
-</div>
